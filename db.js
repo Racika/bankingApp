0c658442fd4b9bd81d08ec1e068ab88263e6ede1
Racika <kalwandvok@gmail.com>
2025-11-06 18:55:21 +0100

    render api

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.PGHOST,
-  port:     process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user:     process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  max: 10,           // connection pool size
-  idleTimeoutMillis: 30000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } 
 });
 
 pool.on('error', (err) => {
